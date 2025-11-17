8d0c81caf86727664e66b93fc45e9286b3440506
GeorgesOlivier <oliviergeorges2906@gmail.com>
2025-11-17 16:44:03 -0500

    avancement av JS

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,25 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
     this.init();
+    this.swipercard();
   }
 
   init() {
     Icons.load();
+    Swiper.load();
+  }
+
+  swipercard() {
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
   }
 }
 new Main();
